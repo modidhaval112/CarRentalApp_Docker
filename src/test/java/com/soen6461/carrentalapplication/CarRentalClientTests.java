07a65ba11e6ebe4b40ea1aa7e4766a22d166e1e6
Himalayayadav444 <37230928+Himalayayadav444@users.noreply.github.com>
2019-10-04 13:37:55 -0400

    Update Class CarRentalClientTests.java
    and added few test cases

--- src/test/java/com/soen6461/carrentalapplication/CarRentalClientTests.java
+++ src/test/java/com/soen6461/carrentalapplication/CarRentalClientTests.java
@@ -20,13 +20,12 @@ import java.util.Date;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class CarRentalApplicationTests {
+public class CarRentalClientTests {
 
 	@Autowired
 	private ClientController clientController;
 
-	@Autowired
-	private  VehicleCatalog vehicleCatalog;
+
 
 	@Test
 	public void contextLoads() throws Exception {
@@ -70,48 +69,7 @@ public class CarRentalApplicationTests {
 	}
 
 
-	@Test
-	public void vehicleCheck()  throws Exception{
-		VehicleRecord v1 =  vehicleCatalog.getVehicleRecord("A12_636");
-		VehicleRecord v2 =  vehicleCatalog.getVehicleRecord("X12_646");
-		Assert.assertNotNull(v1);
-		Assert.assertNotNull(v2);
-	}
-	@Test
-
-	public void vehicleRentSizeCheck() throws Exception{
-		VehicleRecord v1 =  vehicleCatalog.getVehicleRecord("A12_636");
-		VehicleRecord v2 =  vehicleCatalog.getVehicleRecord("X12_646");
-		Assert.assertTrue(v1.getVehicleTransactionList().size() != 0);
-		Assert.assertTrue(v2.getVehicleTransactionList().size() != 0);
-	}
-
-	@Test
-
-	public void vehicleNewRent() throws Exception{
-		ClientRecord clientRecord = new ClientRecord("T-1234-123456-12", "Johny", "Tester", "(438) 566-9999", "2059-10-31");
-
-		VehicleRecord v1 =  vehicleCatalog.getVehicleRecord("A12_636");
-		Transaction t1= new Transaction(clientRecord, v1, "2021-09-10", "2021-10-15", Transaction.Status.Rented);
-		v1.addTransaction(t1);
-		v1.getTransactionList().forEach((n) -> System.out.println(n));
-		Assert.assertTrue(v1.getTransactionList().contains(t1));
-	}
-
-	@Test
-	public void vehicleCancel() throws Exception{
-		ClientRecord clientRecord = new ClientRecord("T-1234-123456-12", "Johny", "Tester", "(438) 566-9999", "2059-10-31");
-
-		VehicleRecord v1 =  vehicleCatalog.getVehicleRecord("A12_636");
-		Transaction t1= new Transaction(clientRecord, v1, "2021-09-10", "2021-10-15", Transaction.Status.Rented);
-		v1.addTransaction(t1);
-		int beforecancel= v1.getTransactionList().size();
-		v1.removeTransaction("T-1234-123456-12_A12_636_Fri Sep 10 00:00:00 EDT 2021");
-		int afterecancel= v1.getTransactionList().size();
-
-		Assert.assertNotEquals(beforecancel, afterecancel);
 	
-	}
 	
 }
 
