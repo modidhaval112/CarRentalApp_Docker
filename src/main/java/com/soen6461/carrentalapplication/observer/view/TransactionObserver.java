4fe7631138d17c44d35175c77fa78393454a0cdc
Dheeraj <dheeraj.as008@gmail.com>
2019-10-03 22:00:26 -0400

    Added time-stamp to the log

--- src/main/java/com/soen6461/carrentalapplication/observer/view/TransactionObserver.java
+++ src/main/java/com/soen6461/carrentalapplication/observer/view/TransactionObserver.java
@@ -1,5 +1,7 @@
 package com.soen6461.carrentalapplication.observer.view;
 
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
 import java.util.Observable;
 import java.util.Observer;
 
@@ -14,8 +16,9 @@ public class TransactionObserver   implements Observer {
 	public void update(Observable o, Object arg) {
 		
 		Record r = (Record) o;
+		ZonedDateTime zonedDateTime = ZonedDateTime.now( ZoneId.of( "America/Montreal" ) );
 		System.out.println("---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------");
-		System.out.println("Transaction Observer View :"+"--Status : "+  r.transactionType+ ", Transaction ID : "+r.trans.getTransactionId() +" ,Name : "+r.trans.getClientRecord().getFirstName() +" " + r.trans.getClientRecord().getLastName() +" ,License No : "+ r.trans.getClientRecord().getDriversLicenseNumber() +" ,Vehicle Type :  " +r.trans.getVehicleRecord().getCarType() +" ,Vehicle No : " +r.trans.getVehicleRecord().getLpr());
+		System.out.println("Transaction Observer View :"+"Created at Timestamp: "+zonedDateTime+"--Status : "+  r.transactionType+ ", Transaction ID : "+r.trans.getTransactionId() +" ,Name : "+r.trans.getClientRecord().getFirstName() +" " + r.trans.getClientRecord().getLastName() +" ,License No : "+ r.trans.getClientRecord().getDriversLicenseNumber() +" ,Vehicle Type :  " +r.trans.getVehicleRecord().getCarType() +" ,Vehicle No : " +r.trans.getVehicleRecord().getLpr());
 
 		
 	}
