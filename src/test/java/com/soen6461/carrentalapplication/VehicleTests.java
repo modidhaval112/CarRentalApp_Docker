3b22492e5aac3a907ea65792f9112bb734ff61ca
Sai Krishna <saich94@gmail.com>
2019-10-04 15:39:44 -0400

    Added new test cases

---
+++ src/test/java/com/soen6461/carrentalapplication/VehicleTests.java
@@ -0,0 +1,105 @@
+package com.soen6461.carrentalapplication;
+
+import com.soen6461.carrentalapplication.controller.ClientController;
+
+import static org.assertj.core.api.Assertions.*;
+
+import com.soen6461.carrentalapplication.controller.VehicleCatalog;
+import com.soen6461.carrentalapplication.model.ClientRecord;
+import com.soen6461.carrentalapplication.model.Transaction;
+import com.soen6461.carrentalapplication.model.VehicleRecord;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class VehicleTests {
+	
+	@Autowired
+	private  VehicleCatalog vehicleCatalog;
+	@Test
+	public void vehicleCheck()  throws Exception{
+		VehicleRecord v1 =  vehicleCatalog.getVehicleRecord("A12_636");
+		VehicleRecord v2 =  vehicleCatalog.getVehicleRecord("X12_646");
+		Assert.assertNotNull(v1);
+		Assert.assertNotNull(v2);
+	}
+	@Test
+
+	public void vehicleRentSizeCheck() throws Exception{
+		VehicleRecord v1 =  vehicleCatalog.getVehicleRecord("A12_636");
+		VehicleRecord v2 =  vehicleCatalog.getVehicleRecord("X12_646");
+		Assert.assertTrue(v1.getVehicleTransactionList().size() != 0);
+		Assert.assertTrue(v2.getVehicleTransactionList().size() != 0);
+	}
+
+	public void vehicleNewRent() throws Exception{
+		ClientRecord clientRecord = new ClientRecord("T-1234-123456-12", "Johny", "Tester", "(438) 566-9999", "2059-10-31");
+
+		VehicleRecord v1 =  vehicleCatalog.getVehicleRecord("A12_636");
+		Transaction t1= new Transaction(clientRecord, v1, "2021-09-10", "2021-10-15", Transaction.Status.Rented);
+		v1.addTransaction(t1);
+		v1.getTransactionList().forEach((n) -> System.out.println(n));
+		Assert.assertTrue(v1.getTransactionList().contains(t1));
+	}
+	@Test
+	public void vehicleReserve() throws Exception{
+		ClientRecord clientRecord = new ClientRecord("T-1234-123456-12", "Johny", "Tester", "(438) 566-9999", "2059-10-31");
+
+		VehicleRecord v1 =  vehicleCatalog.getVehicleRecord("A12_636");
+		Transaction t1= new Transaction(clientRecord, v1, "2022-09-10", "2022-10-15", Transaction.Status.Rented);
+		v1.addTransaction(t1);
+		v1.getTransactionList().forEach((n) -> System.out.println(n));
+		Assert.assertTrue(v1.getTransactionList().contains(t1));
+	}
+	@Test
+	public void vehicleCancel() throws Exception{
+		ClientRecord clientRecord = new ClientRecord("T-1234-123456-12", "Johny", "Tester", "(438) 566-9999", "2059-10-31");
+
+		VehicleRecord v1 =  vehicleCatalog.getVehicleRecord("A12_636");
+		Transaction t1= new Transaction(clientRecord, v1, "2021-09-10", "2021-10-15", Transaction.Status.Rented);
+		v1.addTransaction(t1);
+		int beforecancel= v1.getTransactionList().size();
+		v1.removeTransaction("T-1234-123456-12_A12_636_Fri Sep 10 00:00:00 EDT 2021");
+		int afterecancel= v1.getTransactionList().size();
+
+		Assert.assertNotEquals(beforecancel, afterecancel);
+	
+	}
+	
+	@Test
+	public void vehicleReturn() throws Exception{
+		ClientRecord clientRecord = new ClientRecord("T-1234-123456-12", "Johny", "Tester", "(438) 566-9999", "2059-10-31");
+
+		VehicleRecord v1 =  vehicleCatalog.getVehicleRecord("A12_636");
+		Transaction t1= new Transaction(clientRecord, v1, "2021-09-10", "2021-10-15", Transaction.Status.Rented);
+		v1.addTransaction(t1);
+		int beforecancel= v1.getTransactionList().size();
+		v1.removeTransaction("T-1234-123456-12_A12_636_Fri Sep 10 00:00:00 EDT 2021");
+		int afterecancel= v1.getTransactionList().size();
+
+		Assert.assertNotEquals(beforecancel, afterecancel);
+	
+	}
+	
+	@Test
+	public void vehicleReturnCheck() throws Exception{
+		
+		ClientRecord clientRecord = new ClientRecord("T-1234-123456-12", "Johny", "Tester", "(438) 566-9999", "2059-10-31");
+
+		VehicleRecord v1 =  vehicleCatalog.getVehicleRecord("A12_636");
+		Transaction t1= new Transaction(clientRecord, v1, "2022-09-10", "2021-10-15", Transaction.Status.Rented);
+		v1.removeTransaction("T-1234-123456-12_A12_636_Fri Sep 10 00:00:00 EDT 2022");
+		v1.getTransactionList().forEach((n) -> System.out.println(n));
+		Assert.assertFalse(v1.getTransactionList().contains(t1));
+		
+		
+		
+	
+	}
+
+}
