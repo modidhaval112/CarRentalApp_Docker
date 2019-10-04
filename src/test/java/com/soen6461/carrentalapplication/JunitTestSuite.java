7d372066df96c40f45ea7750c984afc4b19eaa20
Sai Krishna <saich94@gmail.com>
2019-10-03 20:41:20 -0400

    Added Junit test suite

---
+++ src/test/java/com/soen6461/carrentalapplication/JunitTestSuite.java
@@ -0,0 +1,14 @@
+package com.soen6461.carrentalapplication;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+
+@Suite.SuiteClasses({
+   CarRentalApplicationTests.class,
+   FrontendActionTest.class
+})
+
+public class JunitTestSuite  {   
+}  	
