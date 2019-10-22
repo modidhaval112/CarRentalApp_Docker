9be22d8580714b591011a839555eaa7189fed2c0
Martin <martin.morin@hypertherm.com>
2019-10-21 23:18:58 -0400

    Added user

---
+++ src/main/java/com/soen6461/carrentalapplication/config/User.java
@@ -0,0 +1,41 @@
+package com.soen6461.carrentalapplication.config;
+
+public abstract class User {
+
+    private String username;
+
+    // TODO: Encrypt the password when data persistence is used with the database in iteration 4.
+    private String password;
+
+    private RoleType role;
+
+
+    public enum RoleType {
+        Clerk,
+        Administrator
+    }
+
+    /**
+     * User class parameterised constructor.
+     *
+     * @param username username.
+     * @param password password.
+     */
+    protected User(String username, String password, RoleType role) {
+        this.username = username;
+        this.password = password;
+        this.role = role;
+    }
+
+    String getUsername() {
+        return this.username;
+    }
+
+    String getPassword() {
+        return this.password;
+    }
+
+    RoleType getRole() {
+        return this.role;
+    }
+}
