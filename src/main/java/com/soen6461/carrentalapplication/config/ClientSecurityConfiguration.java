8474c2150cf348ae5df8805ce4a84f3248f80fe3
Himalayayadav444 <37230928+Himalayayadav444@users.noreply.github.com>
2019-10-22 19:25:31 -0400

    Added user and administrator configuration

--- src/main/java/com/soen6461/carrentalapplication/config/ClientSecurityConfiguration.java
+++ src/main/java/com/soen6461/carrentalapplication/config/ClientSecurityConfiguration.java
@@ -1,6 +1,5 @@
 package com.soen6461.carrentalapplication.config;
 
-import javax.sql.DataSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -9,62 +8,88 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+import java.util.ArrayList;
+import java.util.Collection;
 
 @Configuration
 @EnableWebSecurity
 public class ClientSecurityConfiguration extends WebSecurityConfigurerAdapter {
+	
+	
+	@Autowired
+	private SimpleAuthenticationSuccessHandler successHandler;
 
-	/*
-	 * @Autowired DataSource dataSource;
-	 */
+	@Autowired
+	UserRegister userRegister;
 
 	@Bean
 	public static NoOpPasswordEncoder passwordEncoder() {
 		return (NoOpPasswordEncoder) NoOpPasswordEncoder.getInstance();
 	}
 
-	// Enable jdbc authentication
-	/*
-	 * @Autowired public void configAuthentication(AuthenticationManagerBuilder
-	 * auth) throws Exception { auth.jdbcAuthentication().dataSource(dataSource); }
-	 */
-
 	@Override
 	public void configure(WebSecurity web) throws Exception {
 		web.ignoring().antMatchers("/resources/**");
 	}
 
-	
-	/*
-	 * @Override protected void configure(HttpSecurity http) throws Exception {
-	 * http.authorizeRequests().antMatchers("/").permitAll().antMatchers(
-	 * "/vehicle-catalog") .hasAnyRole("USER",
-	 * "ADMIN").antMatchers("/getClients").hasAnyRole("USER", "ADMIN")
-	 * .antMatchers("/addNewClient").hasAnyRole("ADMIN").anyRequest().authenticated(
-	 * ).and().formLogin()
-	 * .loginPage("/login").permitAll().and().logout().permitAll().and() // added
-	 * .httpBasic(); ;
-	 * 
-	 * http.formLogin().defaultSuccessUrl("/vehicle-catalog", true);
-	 * http.csrf().disable(); }
-	 */
-	 
-
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http.authorizeRequests().antMatchers("/").permitAll().antMatchers("/vehicle-catalog")
-				.hasAnyRole("USER", "ADMIN").antMatchers("/getClients").hasAnyRole("USER", "ADMIN")
-				.antMatchers("/addNewClient").hasAnyRole("ADMIN").anyRequest().authenticated().and().formLogin()
-				.loginPage("/login").permitAll().and().logout().permitAll().and().httpBasic();
-		http.formLogin().defaultSuccessUrl("/vehicle-catalog", true);
+		http.csrf().requireCsrfProtectionMatcher(new AntPathRequestMatcher("/")).and().authorizeRequests()
+				.antMatchers("/vehicle-catalog").hasAnyRole("USER", User.RoleType.Clerk.name(), "ADMIN", User.RoleType.Administrator.name())
+				.antMatchers("/client-register").hasAnyRole("USER", User.RoleType.Clerk.name())
+				.antMatchers("/client-sign-up").hasAnyRole("USER", User.RoleType.Clerk.name())
+				.antMatchers("/client-update").hasAnyRole("USER", User.RoleType.Clerk.name())
+				.antMatchers("/online-help").hasAnyRole("USER", User.RoleType.Clerk.name(),"ADMIN", User.RoleType.Administrator.name())
+				.antMatchers("/trans-list").hasAnyRole("ADMIN", User.RoleType.Administrator.name())
+				.antMatchers("/vehicle-add").hasAnyRole("ADMIN", User.RoleType.Administrator.name())
+				.antMatchers("/vehicle-register").hasAnyRole("ADMIN", User.RoleType.Administrator.name())
+				.antMatchers("/edit-vehicle").hasAnyRole("ADMIN", User.RoleType.Administrator.name())
+				.anyRequest().authenticated().and().formLogin().successHandler(successHandler)
+				.loginPage("/login").permitAll().and().logout().permitAll().and().httpBasic()
+				.and().exceptionHandling().accessDeniedPage("/accessDenied.jsp");
+				// https://www.baeldung.com/spring-security-custom-access-denied-page
+
+		http.formLogin().defaultSuccessUrl("/", true);
 		http.csrf().disable();
 	}
 
-	@Autowired
-	public void configureGlobal(AuthenticationManagerBuilder authenticationMgr) throws Exception {
-		authenticationMgr.inMemoryAuthentication().withUser("admin").password("admin").authorities("ROLE_USER")
-				.and().withUser("clerk").password("clerk").authorities("ROLE_USER", "ROLE_ADMIN");
+	@Override
+	protected void configure(AuthenticationManagerBuilder authenticationMgr) throws Exception {
+		authenticationMgr.userDetailsService(inMemoryUserDetailsManager());
 	}
 
+	@Bean
+	public InMemoryUserDetailsManager inMemoryUserDetailsManager() {
+
+		// https://spring.io/guides/gs/securing-web/
+
+		Collection<UserDetails> userDetailsCollection = new ArrayList<UserDetails>();
+
+		// Add all clerks
+		for (Clerk clerk : this.userRegister.getAllClerks()) {
+			UserDetails userDetails = org.springframework.security.core.userdetails.User.withUsername(clerk.getUsername())
+							.password(clerk.getPassword())
+							.roles(clerk.getRole().name())
+							.build();
+
+			userDetailsCollection.add(userDetails);
+		}
+
+		// Add all administrators
+		for (Administrator administrator : this.userRegister.getAllAdministrators()) {
+			UserDetails userDetails = org.springframework.security.core.userdetails.User.withUsername(administrator.getUsername())
+					.password(administrator.getPassword())
+					.roles(administrator.getRole().name())
+					.build();
+
+			userDetailsCollection.add(userDetails);
+		}
+
+		return new InMemoryUserDetailsManager(userDetailsCollection);
+	}
 }
