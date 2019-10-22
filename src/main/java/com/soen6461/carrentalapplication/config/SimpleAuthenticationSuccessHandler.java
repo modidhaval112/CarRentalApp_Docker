9d4f20a91b8b1e1f17e249d66c880451c40a79ab
RealNameHidden <dheeraj.as008@gmail.com>
2019-10-22 09:13:45 -0400

    Add simple authentication success handler

---
+++ src/main/java/com/soen6461/carrentalapplication/config/SimpleAuthenticationSuccessHandler.java
@@ -0,0 +1,46 @@
+package com.soen6461.carrentalapplication.config;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.web.DefaultRedirectStrategy;
+import org.springframework.security.web.RedirectStrategy;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Collection;
+
+@Component
+public class SimpleAuthenticationSuccessHandler implements AuthenticationSuccessHandler {
+	
+	private RedirectStrategy redirectStrategy = new DefaultRedirectStrategy();
+	
+	@Override
+	public void onAuthenticationSuccess(HttpServletRequest arg0, HttpServletResponse arg1, Authentication authentication)
+			throws IOException, ServletException {
+		
+		Collection< ? extends GrantedAuthority> authorities = authentication.getAuthorities();
+		authorities.forEach(authority -> {
+			if(authority.getAuthority().equals("ROLE_USER") || authority.toString().equals("ROLE_" + User.RoleType.Clerk)) {
+				try {
+					redirectStrategy.sendRedirect(arg0, arg1, "/vehicle-catalog");
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			} else if(authority.getAuthority().equals("ROLE_ADMIN") || authority.toString().equals("ROLE_" + User.RoleType.Administrator)) {
+				try {
+					redirectStrategy.sendRedirect(arg0, arg1, "/vehicle-register");
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			} else {
+	            throw new IllegalStateException();
+	        }
+		});
+		
+	}
+ 
+}
