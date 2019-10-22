0731f3ce57779f31f182c7639200533de85ed907
manasamurali <m.manasamurali@gmail.com>
2019-10-22 09:44:41 -0400

    Enhanced observer to capture and save transaction history

--- src/main/java/com/soen6461/carrentalapplication/observer/view/TransactionObserver.java
+++ src/main/java/com/soen6461/carrentalapplication/observer/view/TransactionObserver.java
@@ -6,23 +6,29 @@ import java.util.Observable;
 import java.util.Observer;
 
 import com.soen6461.carrentalapplication.model.Record;
+import com.soen6461.carrentalapplication.model.TransactionHistory;
 
-
-public class TransactionObserver   implements Observer {
-
-	
+public class TransactionObserver implements Observer {
 
 	@Override
 	public void update(Observable o, Object arg) {
-		
+
 		Record r = (Record) o;
-		ZonedDateTime zonedDateTime = ZonedDateTime.now( ZoneId.of( "America/Montreal" ) );
+		ZonedDateTime timeStamp = ZonedDateTime.now(ZoneId.of("America/Montreal"));
+
+		TransactionHistory transactionHistory = new TransactionHistory(r.transaction, timeStamp, r.transactionType);
+		r.transactionHistory.add(transactionHistory);
+
 		System.out.println("---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------");
-		System.out.println("Transaction Observer View :"+"Created at Timestamp: "+zonedDateTime+"\n Transaction ID : "+r.trans.getTransactionId() +"\nClient Name : "+r.trans.getClientRecord().getFirstName() +" " + r.trans.getClientRecord().getLastName() +", License No : "+ r.trans.getClientRecord().getDriversLicenseNumber() +"\n Status: "+  r.transactionType+ "\n Vehicle Type :  " +r.trans.getVehicleRecord().getCarType() +" with Vehicle No : " +r.trans.getVehicleRecord().getLpr()+" Due Date : " +r.trans.getEndDate());
+		System.out.println("Transaction Observer View :" +
+				"Created at Timestamp: " + timeStamp + "\n" +
+				"Status: " + r.transactionType + "\n" +
+				r.transaction.toString() + "\n" +
+				r.transaction.getClientRecord().toString() + "\n" +
+				r.transaction.getVehicleRecord().toString() + "\n");
 
-		
+		for (int i = 0; i < r.transactionHistory.size(); i++) {
+			System.out.println("No " + i + " " + r.transactionHistory.get(i).getTransaction().getTransactionId() + " " + r.transactionHistory.get(i).getStatus() + " " + r.transactionHistory.get(i).getTransaction().getClientRecord().getFirstName());
+		}
 	}
-	
-	
-
 }
