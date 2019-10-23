5ead3ffe84e55824816ad84a9b6c7ec7ec46a84f
Martin Morin <martin.morin.002@gmail.com>
2019-10-22 23:46:33 -0400

    Adding data validation methods and tests.

---
+++ src/main/java/com/soen6461/carrentalapplication/Helpers/DataValidationHelper.java
@@ -0,0 +1,56 @@
+package com.soen6461.carrentalapplication.Helpers;
+
+public class DataValidationHelper {
+
+    /**
+     * Validates the format of the phone number.
+     *
+     * @param phoneNumber Phone number.
+     * @return True if the phone number is valid, false otherwise.
+     */
+    public static boolean isPhoneNumberFormatValid(String phoneNumber) {
+        // Code was taken from the following web reference: https://www.journaldev.com/641/regular-expression-phone-number-validation-in-java
+
+        try {
+            phoneNumber = phoneNumber.trim();
+
+            //validate phone numbers of format "1234567890"
+            if (phoneNumber.matches("\\d{10}")) {
+                return true;
+            }
+            //validating phone number with -, . or spaces
+            else if (phoneNumber.matches("\\d{3}[-\\.\\s]\\d{3}[-\\.\\s]\\d{4}")) {
+                return true;
+            }
+            //validating phone number with extension length from 3 to 5
+            else if (phoneNumber.matches("\\d{3}-\\d{3}-\\d{4}\\s(x|(ext))\\d{3,5}")) {
+                return true;
+            }
+            //validating phone number where area code is in braces ()
+            else if (phoneNumber.matches("\\(\\d{3}\\)-\\d{3}-\\d{4}")) {
+                return true;
+            }
+        } catch (Exception e){
+            // There was an error and the matching failed.
+        }
+
+        return false;
+    }
+
+    /**
+     * Validate the format of the date. (yyyy-mm-dd)
+     *
+     * @param date a date.
+     * @return True if the date is valid, false otherwise.
+     */
+    public static boolean isDateFormatValid(String date) {
+        date = date.trim();
+
+        if (date.matches("\\d{4}[-]\\d{1,2}[-]\\d{1,2}")) {
+            return true;
+        }
+
+        // No match found
+        return false;
+    }
+}
---
+++ src/test/java/com/soen6461/carrentalapplication/DataValidationHelperTests.java
@@ -0,0 +1,32 @@
+package com.soen6461.carrentalapplication;
+
+import com.soen6461.carrentalapplication.Helpers.DataValidationHelper;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class DataValidationHelperTests {
+
+    /**
+     * Test the format of phone numbers.
+     */
+    @Test
+    public void phoneNumberFormatValidation() {
+        Assert.assertFalse(DataValidationHelper.isPhoneNumberFormatValid("Fake number"));
+        Assert.assertTrue(DataValidationHelper.isPhoneNumberFormatValid("1234567890"));
+        Assert.assertTrue(DataValidationHelper.isPhoneNumberFormatValid("123-456-7890 "));
+        Assert.assertTrue(DataValidationHelper.isPhoneNumberFormatValid("123-456-7890 x1234"));
+        Assert.assertTrue(DataValidationHelper.isPhoneNumberFormatValid("123-456-7890 ext1234"));
+        Assert.assertTrue(DataValidationHelper.isPhoneNumberFormatValid("(123)-456-7890"));
+        Assert.assertTrue(DataValidationHelper.isPhoneNumberFormatValid("123.456.7890"));
+        Assert.assertTrue(DataValidationHelper.isPhoneNumberFormatValid("123 456 7890"));
+    }
+
+    /**
+     * Test the format of the date expected for this application.
+     */
+    @Test
+    public void DateFormatValidation() {
+        Assert.assertFalse(DataValidationHelper.isDateFormatValid("Fake number"));
+        Assert.assertTrue(DataValidationHelper.isDateFormatValid("2013-10-05"));
+    }
+}
