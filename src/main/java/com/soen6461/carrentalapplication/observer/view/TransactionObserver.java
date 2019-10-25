de758dd5342f2d4df31159fc66393097b14367ed
Martin Morin <martin.morin.002@gmail.com>
2019-10-24 21:52:59 -0400

    Fix #35 time-stamp separation of concern.

--- src/main/java/com/soen6461/carrentalapplication/observer/view/TransactionObserver.java
+++ src/main/java/com/soen6461/carrentalapplication/observer/view/TransactionObserver.java
@@ -1,7 +1,5 @@
 package com.soen6461.carrentalapplication.observer.view;
 
-import java.time.ZoneId;
-import java.time.ZonedDateTime;
 import java.util.Observable;
 import java.util.Observer;
 
@@ -14,14 +12,13 @@ public class TransactionObserver implements Observer {
 	public void update(Observable o, Object arg) {
 
 		Record r = (Record) o;
-		ZonedDateTime timeStamp = ZonedDateTime.now(ZoneId.of("America/Montreal"));
 
-		TransactionHistory transactionHistory = new TransactionHistory(r.transaction, timeStamp, r.transactionType);
+		TransactionHistory transactionHistory = new TransactionHistory(r.transaction, r.transactionType);
 		r.transactionHistory.add(transactionHistory);
 
 		System.out.println("---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------");
 		System.out.println("Transaction Observer View :" +
-				"Created at Timestamp: " + timeStamp + "\n" +
+				"Created at Timestamp: " + transactionHistory.getTimeStamp() + "\n" +
 				"Status: " + r.transactionType + "\n" +
 				r.transaction.toString() + "\n" +
 				r.transaction.getClientRecord().toString() + "\n" +
