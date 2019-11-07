d277a1e1acbeb523de7104c913e828a635f53d2e
Dheeraj <dheeraj.as008@gmail.com>
2019-11-07 01:49:42 -0500

    Implemented Jdbc Template in TDG class

--- src/main/java/com/soen6461/carrentalapplication/tabledatagateway/ClientRecordTdg.java
+++ src/main/java/com/soen6461/carrentalapplication/tabledatagateway/ClientRecordTdg.java
@@ -1,22 +1,27 @@
 package com.soen6461.carrentalapplication.tabledatagateway;
 
+import com.soen6461.carrentalapplication.mapper.ClientRecordDataMapper;
 import com.soen6461.carrentalapplication.model.ClientRecord;
 
 import java.sql.*;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
 
 import javax.sql.DataSource;
+import java.util.List;
 import java.util.Properties;
 
+@Repository
 public class ClientRecordTdg {
 
     @Autowired
     DataSource dataSource;
-    boolean isDataMapperTest;
 
-    private Connection connection;
+    @Autowired
+    JdbcTemplate jdbcTemplate;
 
     /**
      * Insert a new record in the database to persist its data.
@@ -26,14 +31,14 @@ public class ClientRecordTdg {
     //@Override
     public boolean insert(ClientRecord clientRecordToInsert) {
         String sql = "CREATE TABLE IF NOT EXISTS `carrentaldb`.`" + this.getClientRecordTableName() + "` (" +
-                "    `driversLicenseNumber` String PRIMARY KEY," +
+                "    `driversLicenseNumber` VARCHAR(50) PRIMARY KEY," +
                 "    `version` INT," +
                 "    `firstname` VARCHAR(60)," +
                 "    `lastname` VARCHAR(50)," +
                 "    `phoneNumber` VARCHAR(50)," +
-                "    `expirationDate` Date(50)," +
+                "    `expirationDate` Date" +
                 ");";
-        if (this.sqlUpdateStatement(sql)) {
+
             String sqlRecord = "INSERT INTO `carrentaldb`.`" + this.getClientRecordTableName() + "`" +
                     "(`driversLicenseNumber`, `version`, `firstname`, `lastname`, `phoneNumber`, `expirationDate`) " +
                     "VALUES (" +
@@ -44,10 +49,15 @@ public class ClientRecordTdg {
                     "\"" + clientRecordToInsert.getPhoneNumber() + "\", " +
                     "\"" + clientRecordToInsert.getExpirationDate() + "\", " +
                     ");";
-            return this.sqlUpdateStatement(sqlRecord);
-        }
+            try{
+                jdbcTemplate.execute(sql);
+            jdbcTemplate.execute(sqlRecord);
+            } catch (DataIntegrityViolationException e) {
+                e.printStackTrace();
+                return false;
+            }
+        return true;
 
-        return false;
     }
 
     /**
@@ -56,9 +66,9 @@ public class ClientRecordTdg {
      * @param driversLicenseNumber Driver's license number of the record to delete from the database.
      */
     //@Override
-    public boolean delete(String driversLicenseNumber) {
-        String sql = "DELETE from `carrentaldb`.`" + this.getClientRecordTableName() + "` where driversLicenseNumber = '" + driversLicenseNumber + "'";
-        return this.sqlUpdateStatement(sql);
+    public void delete(String driversLicenseNumber) {
+        String sql = "DELETE from `carrentaldb`.`" + this.getClientRecordTableName() + "`   WHERE `driversLicenseNumber` = '" + driversLicenseNumber + "'";
+        jdbcTemplate.execute(sql);
     }
 
     /**
@@ -67,7 +77,7 @@ public class ClientRecordTdg {
      * @param objectToUpdate Object to update.
      */
     //@Override
-    public boolean update(ClientRecord objectToUpdate) {
+    public void update(ClientRecord objectToUpdate) {
         String sql = " UPDATE  `carrentaldb`.`" + this.getClientRecordTableName() + "` SET " +
                 "`driversLicenseNumber`=" +  objectToUpdate.getDriversLicenseNumber() + ", " +
                 "`version`=" + objectToUpdate.getRecordVersion() + ", " +
@@ -76,7 +86,7 @@ public class ClientRecordTdg {
                 "`phoneNumber`=\"" + objectToUpdate.getPhoneNumber() + "\", " +
                 "`expirationDate`=\"" + objectToUpdate.getExpirationDate() + "\", " +
                 " WHERE driversLicense=" + objectToUpdate.getDriversLicenseNumber() +";";
-        return this.sqlUpdateStatement(sql);
+        jdbcTemplate.execute(sql);
     }
 
     /**
@@ -89,11 +99,8 @@ public class ClientRecordTdg {
     public ResultSet getObject(String driversLicenseNumber) {
         String sql = "SELECT * FROM `carrentaldb`.`" + this.getClientRecordTableName() + "` WHERE driversLicenseNumber = " + driversLicenseNumber;
 
-        try (Statement stmt = this.connection.createStatement()) {
-            ResultSet rs = stmt.executeQuery(sql);
-
-            return rs;
-
+        try  {
+            jdbcTemplate.execute(sql);
         } catch (Exception e) {
             System.out.println("Get object exception" + e.getMessage());
         }
@@ -101,29 +108,9 @@ public class ClientRecordTdg {
         return null;
     }
 
-    public ResultSet getAllObjects() {
-        String sql = "SELECT * FROM `carrentaldb`.`" + this.getClientRecordTableName();
-
-        try (Statement stmt = this.connection.createStatement()) {
-
-            ResultSet rs = stmt.executeQuery(sql);
-
-            return rs;
-
-        } catch (Exception e) {
-            System.out.println("Get object exception" + e.getMessage());
-        }
-
-        return null;
-    }
-
-    /**
-     * Method used to allow testing of te vehicle data mapper.
-     *
-     * @param isTest Set to true when testing the data mapper.
-     */
-    public void setIsDataMapperTest(Boolean isTest) {
-        this.isDataMapperTest = isTest;
+    public List<ClientRecord> getAllObjects() {
+        List<ClientRecord> results = jdbcTemplate.query("SELECT * FROM carrentaldb.clientRecord", new ClientRecordDataMapper());
+        return results;
     }
 
     /**
@@ -131,70 +118,10 @@ public class ClientRecordTdg {
      *
      * @return True if the testing data is cleared, false otherwise.
      */
-    public boolean clearTestingData() {
-        String sql = "DROP TABLE IF EXISTS `carrentaldb`.`VehicleRecordTestTable`";
-        return this.sqlUpdateStatement(sql);
-    }
 
-    /**
-     * Execute an update statement on the database.
-     *
-     * @param sql The sql statement.
-     * @return True if the statement was executed without issues, false otherwise.
-     */
-    private boolean sqlUpdateStatement(String sql) {
-
-        if(this.connection == null)
-        {
-            try {
-                this.getConnection();
-            } catch (Exception e){
-                System.out.println("Exception in connecting to the database");
-                e.printStackTrace();
-            }
-        }
-
-        boolean isSuccess;
-        Statement statement;
-
-        try {
-            statement = this.connection.createStatement();
-
-            statement.executeUpdate(sql);
-
-            // Releasing the resources
-            if (statement != null) {
-                statement.close();
-            }
-
-            isSuccess = true;
-        } catch (Exception exception) {
-            System.out.println(exception.getMessage());
-            System.out.println(sql);
-            isSuccess = false;
-        }
-
-        return isSuccess;
-    }
-
-    /**
-     * Gets the connection to the database.
-     *
-     */
-    public void getConnection() throws SQLException {
-        try{
-        this.connection = this.dataSource.getConnection();
-        } catch (Exception exception) {
-            System.out.println(exception.getMessage());
-        }
-
-    }
 
 
     private String getClientRecordTableName() {
-        if (this.isDataMapperTest) {
-            return "ClientRecordTestTable";
-        }
 
         return "ClientRecord";
     }
