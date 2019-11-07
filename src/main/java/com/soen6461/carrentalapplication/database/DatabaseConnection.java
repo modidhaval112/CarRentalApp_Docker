363622e0397ed9ba5a23fab871518604aad3d79f
Sai Krishna <saich94@gmail.com>
2019-11-07 02:26:15 -0500

    database connection setup

---
+++ src/main/java/com/soen6461/carrentalapplication/database/DatabaseConnection.java
@@ -0,0 +1,32 @@
+package com.soen6461.carrentalapplication.database;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class DatabaseConnection {
+	@Autowired
+	private DataSource dataSource;
+
+	private static DatabaseConnection instance = new DatabaseConnection();
+
+	private DatabaseConnection(){}
+	public static DatabaseConnection getInstance(){
+		return instance;
+	}
+
+
+	public void setDataSource(DataSource dataSource) {
+		this.dataSource = dataSource;
+	}
+
+	public Connection getConnection() throws SQLException
+	{
+		return dataSource.getConnection();
+	}
+
+
+}
