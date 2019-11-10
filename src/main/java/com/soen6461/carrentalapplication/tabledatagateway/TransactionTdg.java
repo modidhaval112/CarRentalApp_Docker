efb8d0d75c6e90a96692c03fab20b04b34dcf54d
Himalayayadav444 <37230928+Himalayayadav444@users.noreply.github.com>
2019-11-09 20:38:31 -0500

    minor changes

--- src/main/java/com/soen6461/carrentalapplication/tabledatagateway/TransactionTdg.java
+++ src/main/java/com/soen6461/carrentalapplication/tabledatagateway/TransactionTdg.java
@@ -15,7 +15,9 @@ import java.util.Map;
 import javax.sql.DataSource;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public class TransactionTdg {
 	
     @Autowired
