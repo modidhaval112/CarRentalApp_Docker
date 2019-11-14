897e79ef9233912503ed97f210311a81a55cc212
Sai Krishna <saich94@gmail.com>
2019-11-13 22:27:59 -0500

    added register and commit methods

--- src/main/java/com/soen6461/carrentalapplication/unitofwork/TransactionRepository.java
+++ src/main/java/com/soen6461/carrentalapplication/unitofwork/TransactionRepository.java
@@ -1,19 +1,25 @@
 package com.soen6461.carrentalapplication.unitofwork;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import com.soen6461.carrentalapplication.mapper.TransactionDataMapper;
 import com.soen6461.carrentalapplication.model.Transaction;
+import com.soen6461.carrentalapplication.model.VehicleRecord;
 
 @Component
 public class TransactionRepository implements IUnitOfWork<Transaction> {
     HashMap<String, List<Transaction>> context = new HashMap<String, List<Transaction>>();
+    private static final Logger LOGGER = LoggerFactory.getLogger(TransactionDataMapper.class);
+
     private Map<String, Boolean> dirtyMap = new HashMap<>();
     private LinkedList<String> deleteRecords = new LinkedList<>();
     private LinkedList<String> deletedTransactionRecords = new LinkedList<>();
@@ -47,41 +53,89 @@ public class TransactionRepository implements IUnitOfWork<Transaction> {
 	public void setDeletedTransactionRecords(LinkedList<String> deletedTransactionRecords) {
 		this.deletedTransactionRecords = deletedTransactionRecords;
 	}
+	
+	
 
-	@Override
-	public boolean commit() {
-		// TODO Auto-generated method stub
-		return false;
-	}
+	 @Override
+	    public void registerNew(Transaction transaction) {
+	        LOGGER.info("Registering {} for insert in context.", transaction.getTransactionId());
+	        register(transaction, IUnitOfWork.INSERT);
+	    }
 
-	@Override
-	public void registerDeleted(Transaction obj) {
-		// TODO Auto-generated method stub
-		
-	}
+	    @Override
+	    public void registerDirty(Transaction transaction) {
+	        LOGGER.info("Registering {} for modify in context.", transaction.getLpr());
+	        register(transaction, IUnitOfWork.MODIFY);
+	    }
 
-	@Override
-	public void registerDirty(Transaction obj) {
-		// TODO Auto-generated method stub
-		
-	}
+	    @Override
+	    public void registerDeleted(Transaction transaction) {
+	        LOGGER.info("Registering {} for delete in context.", transaction.getLpr());
+	        register(transaction, IUnitOfWork.DELETE);
+	    }
 
-	@Override
-	public void registerNew(Transaction obj) {
-		// TODO Auto-generated method stub
-		
-	}
+	    private void register(Transaction transaction, String operation) {
+	        List transactionsToOperate = context.get(operation); // Retrieve list of vehicles that are newly registered to get
+	        if (transactionsToOperate == null) {
+	        	transactionsToOperate = new ArrayList<>();
+	        }
+	        transactionsToOperate.add(transaction);
+	        context.put(operation, transactionsToOperate); // Old list is replaced with new list
+	    }
 
-	@Override
-	public void registerClean(Transaction obj) {
-		// TODO Auto-generated method stub
-		
-	}
+    /**
+     * All UnitOfWork operations are batched and executed together on commit only.
+     */
+    @Override
+    public boolean commit() {
+        if (context == null || context.size() == 0) {
+            return false;
+        }
+        LOGGER.info("Commit started");
+        if (context.containsKey(IUnitOfWork.INSERT)) {
+            commitInsert();
+        }
 
-	@Override
-	public boolean rollback() {
-		// TODO Auto-generated method stub
-		return false;
-	}
+        if (context.containsKey(IUnitOfWork.MODIFY)) {
+            commitModify();
+        }
+        if (context.containsKey(IUnitOfWork.DELETE)) {
+            commitDelete();
+        }
+        context = new HashMap<String, List<TransactionRecord>>();
+        LOGGER.info("Commit finished.");
+        return true;
+    }
+
+    private void commitInsert() {
+        List<TransactionRecord> transactionsToBeInserted = context.get(IUnitOfWork.INSERT);
+        for (TransactionRecord transactionRecord : transactionsToBeInserted) {
+            LOGGER.info("Saving {} to database.", transactionRecord.getLpr());
+            transactionRecordDataMapper.insert(transactionRecord);
+        }
+    }
+
+    private void commitModify() {
+        List<TransactionRecord> modifiedClients = context.get(IUnitOfWork.MODIFY);
+        for (TransactionRecord transactionRecord : modifiedClients) {
+            LOGGER.info("Modifying {} to database.", transactionRecord.getLpr());
+            transactionRecordDataMapper.update(transactionRecord);
+        }
+
+        this.dirtyMap = new HashMap<>();
+    }
+
+    private void commitDelete() {
+        List<Transaction> deletedTransactions = context.get(IUnitOfWork.DELETE);
+        for (Transaction transactionRecord : deletedTransactions) {
+            LOGGER.info("Deleting {} to database.", transactionRecord.getLpr());
+            transactionDataMapper.delete(transactionRecord.getTransactionId());
+            deletedTransactionRecords.add(transactionRecord.getLpr());
+        }
+        this.deleteRecords = new LinkedList<>();
+    }
+
+	
+
 
 }
