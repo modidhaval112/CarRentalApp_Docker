03a498f2bcc5f4454b0e8b2281739b18bebac7fa
saich9424 <saich94@gmail.com>
2019-11-15 18:31:18 -0500

    change date time stamp field

--- src/main/java/com/soen6461/carrentalapplication/observer/view/TransactionObserver.java
+++ src/main/java/com/soen6461/carrentalapplication/observer/view/TransactionObserver.java
@@ -1,5 +1,7 @@
 package com.soen6461.carrentalapplication.observer.view;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.Observable;
 import java.util.Observer;
 
@@ -12,8 +14,10 @@ import com.soen6461.carrentalapplication.model.TransactionHistory;
  *
  * @author Admin
  */
-public class TransactionObserver implements Observer {
 
+public class TransactionObserver implements Observer {
+	DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
+	LocalDateTime now = LocalDateTime.now();
     /**
      * This method will be notified whenever there is any changes in states
      * of observable classes
@@ -26,7 +30,7 @@ public class TransactionObserver implements Observer {
 
         Record r = (Record) o;
 
-        TransactionHistory transactionHistory = new TransactionHistory(r.transaction, r.transactionType);
+        TransactionHistory transactionHistory = new TransactionHistory(r.transaction, r.transactionType,dtf.format(now).toString());
         r.transactionHistory.add(transactionHistory);
 
         System.out.println("---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------");
