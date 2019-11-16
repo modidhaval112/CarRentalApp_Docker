0d5581973825715fdd071940cbfa13ec977f9fd6
Himalayayadav444 <37230928+Himalayayadav444@users.noreply.github.com>
2019-11-15 19:04:57 -0500

    Create TransactionHistoryMapper.java

---
+++ src/main/java/com/soen6461/carrentalapplication/mapper/TransactionHistoryMapper.java
@@ -0,0 +1,46 @@
+package com.soen6461.carrentalapplication.mapper;
+
+import java.text.ParseException;
+import java.util.List;
+
+import com.soen6461.carrentalapplication.model.TransactionHistory;
+
+public class TransactionHistoryMapper implements IDataMapper<TransactionHistory>{
+
+	@Override
+	public boolean save(TransactionHistory objectToSave) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean insert(TransactionHistory objectToInsert) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean update(TransactionHistory objectToUpdate) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean delete(String id) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public TransactionHistory find(String id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<TransactionHistory> findAll() throws ParseException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
