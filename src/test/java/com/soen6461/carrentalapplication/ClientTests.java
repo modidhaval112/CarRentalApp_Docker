322d28c253ddaf62690a3cf21dc99165845cd263
saich9424 <saich94@gmail.com>
2019-11-18 13:00:35 -0500

    Added JUNIT tests

--- src/test/java/com/soen6461/carrentalapplication/ClientTests.java
+++ src/test/java/com/soen6461/carrentalapplication/ClientTests.java
@@ -1,73 +1,103 @@
-/*
- * package com.soen6461.carrentalapplication;
- * 
- * import com.soen6461.carrentalapplication.controller.ClientController;
- * 
- * import static org.assertj.core.api.Assertions.*;
- * 
- * import com.soen6461.carrentalapplication.model.ClientRecord; import
- * org.junit.Assert; import org.junit.Ignore; import org.junit.Test; import
- * org.junit.runner.RunWith; import
- * org.springframework.beans.factory.annotation.Autowired; import
- * org.springframework.boot.test.context.SpringBootTest; import
- * org.springframework.test.context.junit4.SpringRunner;
- * 
- * @RunWith(SpringRunner.class)
- * 
- * @SpringBootTest public class ClientTests {
- * 
- * @Autowired private ClientController clientController;
- * 
- *//**
+
+package com.soen6461.carrentalapplication;
+
+import com.soen6461.carrentalapplication.controller.ClientController;
+
+import static org.assertj.core.api.Assertions.*;
+
+import com.soen6461.carrentalapplication.model.ClientRecord; import
+org.junit.Assert; import org.junit.Ignore; import org.junit.Test; import
+org.junit.runner.RunWith; import
+org.springframework.beans.factory.annotation.Autowired; import
+org.springframework.boot.test.context.SpringBootTest; import
+org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+
+@SpringBootTest
+public class ClientTests {
+
+	@Autowired 
+	private ClientController clientController;
+
+	/**
 	 * Check for the class loading
 	 */
-/*
- * 
- * @Test public void contextLoads() {
- * assertThat(this.clientController).isNotNull(); }
- * 
- *//**
+
+
+	@Test
+	public void contextLoads() {
+		assertThat(this.clientController).isNotNull(); }
+
+	/**
 	 * Check for the Client size
 	 */
-/*
- * 
- * @Test public void checkClientSize() {
- * Assert.assertTrue(clientController.getAllClientRecord().size() == 5); }
- * 
- *//**
-	 * Check for adding new client
-	 */
-/*
- * 
- * @Test public void addNewClient() throws Exception { ClientRecord clientRecord
- * = new ClientRecord("T-1234-123456-12", "Johny", "Tester", "(438) 566-9999",
- * "2059-10-31"); clientController.addClientRecord(clientRecord);
- * Assert.assertTrue(clientController.getAllClientRecord().size() == 6); }
- * 
- *//**
-	 * Check for the client record
-	 */
-/*
- * 
- * @Test public void clientRecordCheck() throws Exception { ClientRecord
- * clientRecord = new ClientRecord("T-1234-123456-12", "Johny", "Tester",
- * "(438) 566-9999", "2059-10-31");
- * clientController.addClientRecord(clientRecord); clientRecord =
- * clientController.searchClient("T-1234-123456-12");
- * 
- * Assert.assertEquals("T-1234-123456-12",
- * clientRecord.getDriversLicenseNumber()); Assert.assertEquals("Johny",
- * clientRecord.getFirstName()); Assert.assertEquals("Tester",
- * clientRecord.getLastName()); Assert.assertEquals("(438) 566-9999",
- * clientRecord.getPhoneNumber()); }
- * 
- *//**
+
+
+	//	@Test public void checkClientSize() {
+	//		Assert.assertTrue(clientController.getAllClientRecord().size() == 5); }
+	//
+	//	/**
+	//	 * Check for adding new client
+	//	 */
+
+
+	@Test 
+	public void addNewClient() throws Exception { 
+		clientController.deleteClientRecord("T-1234-123456-34");
+		clientController.persistData();
+
+
+		ClientRecord clientRecord
+		= new ClientRecord("T-1234-123456-34", 0, "Johny", "Tester", "(438) 566-9999",	  
+				"2059-10-31"); 
+	int count =clientController.getAllClientRecord().size() ;
+	System.out.println(count);
+	clientController.addClientRecord(clientRecord);
+	clientController.persistData();
+	System.out.println(clientController.getAllClientRecord().size());
+	Assert.assertTrue(clientController.getAllClientRecord().size() == count+1); 
+	}
+
+	
+
+	@Ignore @Test 
+	public void clientRecordCheck() throws Exception { 
+		clientController.deleteClientRecord("T-1234-123456-34");
+		clientController.persistData();
+		ClientRecord
+		clientRecord = new ClientRecord("T-1234-123456-12", 0, "Johny", "Tester",
+				"(438) 566-9999", "2059-10-31");
+
+	clientController.addClientRecord(clientRecord);
+	clientController.persistData();
+
+	clientRecord =
+			clientController.searchClient("T-1234-123456-12");
+
+	Assert.assertEquals("T-1234-123456-12",
+			clientRecord.getDriversLicenseNumber()); 
+	Assert.assertEquals("Johny",
+					clientRecord.getFirstName()); 
+	Assert.assertEquals("Tester",
+							clientRecord.getLastName()); 
+	Assert.assertEquals("(438) 566-9999",
+									clientRecord.getPhoneNumber()); }
+
+	/**
 	 * Check for the deletion of the client record
-	 *//*
-		 * @Test public void deleteClient() throws Exception { ClientRecord clientRecord
-		 * = new ClientRecord("T-1234-123456-12", "Johny", "Tester", "(438) 566-9999",
-		 * "2059-10-31"); clientController.addClientRecord(clientRecord);
-		 * Assert.assertTrue(clientController.getAllClientRecord().size() == 6);
-		 * clientController.deleteClientRecord("T-1234-123456-12");
-		 * Assert.assertTrue(clientController.getAllClientRecord().size() == 5); } }
-		 */
+	 */
+	@Test
+	public void deleteClient() throws Exception {
+		ClientRecord clientRecord
+		= new ClientRecord("T-1234-123456-12", 0, "Johny", "Tester", "(438) 566-9999",
+				"2059-10-31"); clientController.addClientRecord(clientRecord);
+				clientController.persistData();
+
+				
+				int count =clientController.getAllClientRecord().size() ;
+
+				clientController.deleteClientRecord("T-1234-123456-12");
+				clientController.persistData();
+
+				Assert.assertTrue(clientController.getAllClientRecord().size() ==count-1); } }
--- src/test/java/com/soen6461/carrentalapplication/TransactionsTests.java
+++ src/test/java/com/soen6461/carrentalapplication/TransactionsTests.java
@@ -1,8 +1,10 @@
-/*package com.soen6461.carrentalapplication;
+package com.soen6461.carrentalapplication;
 
 import java.text.ParseException;
 import java.util.List;
 
+import com.soen6461.carrentalapplication.controller.ClientController;
+import com.soen6461.carrentalapplication.controller.TransactionCatalog;
 import com.soen6461.carrentalapplication.controller.VehicleCatalog;
 import com.soen6461.carrentalapplication.model.ClientRecord;
 import com.soen6461.carrentalapplication.model.Transaction;
@@ -17,19 +19,42 @@ import org.springframework.test.context.junit4.SpringRunner;
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class TransactionsTests {
-    @Autowired
-    VehicleCatalog vehicleCatalog;
+	@Autowired private VehicleCatalog vehicleCatalog;
+	@Autowired 
+	private ClientController clientController;
+	@Autowired
+	private TransactionCatalog transactionCatalog;
 
     @Test
     public void vehiclesCurrentlyOut() throws ParseException {
         List<VehicleRecord> vr = vehicleCatalog.getCurrentlyOutVehicles();
         int beforeadd = vr.size();
         System.out.println(beforeadd);
-        ClientRecord clientRecord = new ClientRecord("T-1234-123456-12", "Johny", "Tester", "(438) 566-9999", "2059-10-31");
+        
+        clientController.deleteClientRecord("T-1234-123456-34");
+		clientController.persistData();
 
-        VehicleRecord v1 = vehicleCatalog.getVehicleRecord("UDF_126");
-        Transaction t1 = new Transaction(clientRecord, v1, "2019-10-23", "2019-12-27", Transaction.Status.Rented);
+		ClientRecord
+		clientRecord = new ClientRecord("T-1234-123456-34", 0, "Johny", "Tester",
+				"(438) 566-9999", "2059-10-31");
+		clientController.persistData();
+
+		vehicleCatalog.deleteVehicleRecord("ABE_636");
+		vehicleCatalog.deleteVehicleRecord("ABD_636");
+		vehicleCatalog.persistData();
+
+		VehicleRecord vd1 =
+				new VehicleRecord(0, 0, "ABE_636", "Sedan", "Audi", "A8", 2011, "Red");
+		VehicleRecord vd2 =
+				new VehicleRecord(0, 0, "ABD_636", "Sedan", "Audi", "A8", 2011, "Red");
+		vehicleCatalog.persistData();
+
+        VehicleRecord v1 = vehicleCatalog.getVehicleRecord("ABE_636");
+        Transaction t1 = new Transaction(1, clientRecord, v1, "2019-10-23", "2019-12-27", Transaction.Status.Rented);
         v1.addTransaction(t1);
+		vehicleCatalog.persistData();
+
+        transactionCatalog.persistData();
         int afteradd = vehicleCatalog.getCurrentlyOutVehicles().size();
 
         System.out.println(afteradd);
@@ -42,11 +67,30 @@ public class TransactionsTests {
         List<VehicleRecord> vr = vehicleCatalog.getCurrentlyOutVehicles();
         int beforeadd = vr.size();
         System.out.println(beforeadd);
-        ClientRecord clientRecord = new ClientRecord("T-1234-123456-12", "Johny", "Tester", "(438) 566-9999", "2059-10-31");
+        clientController.deleteClientRecord("T-1234-123456-34");
+      		clientController.persistData();
 
-        VehicleRecord v1 = vehicleCatalog.getVehicleRecord("UDF_126");
-        Transaction t1 = new Transaction(clientRecord, v1, "2019-12-23", "2019-12-27", Transaction.Status.Rented);
+      		ClientRecord
+      		clientRecord = new ClientRecord("T-1234-123456-34", 0, "Johny", "Tester",
+      				"(438) 566-9999", "2059-10-31");
+      		clientController.persistData();
+
+      		vehicleCatalog.deleteVehicleRecord("ABE_636");
+      		vehicleCatalog.deleteVehicleRecord("ABD_636");
+      		vehicleCatalog.persistData();
+
+      		VehicleRecord vd1 =
+      				new VehicleRecord(0, 0, "ABE_636", "Sedan", "Audi", "A8", 2011, "Red");
+      		VehicleRecord vd2 =
+      				new VehicleRecord(0, 0, "ABD_636", "Sedan", "Audi", "A8", 2011, "Red");
+      		vehicleCatalog.persistData();
+
+              VehicleRecord v1 = vehicleCatalog.getVehicleRecord("ABE_636");
+        Transaction t1 = new Transaction(0, clientRecord, v1, "2019-12-23", "2019-12-27", Transaction.Status.Rented);
         v1.addTransaction(t1);
+        vehicleCatalog.persistData();
+
+        transactionCatalog.persistData();
         int afteradd = vehicleCatalog.getCurrentlyOutVehicles().size();
 
         System.out.println(afteradd);
@@ -59,11 +103,30 @@ public class TransactionsTests {
         List<VehicleRecord> vr = vehicleCatalog.getAvailabilityBetweenDates("2019-12-23", "2019-12-27");
         int beforeadd = vr.size();
         System.out.println(beforeadd);
-        ClientRecord clientRecord = new ClientRecord("T-1234-123456-12", "Johny", "Tester", "(438) 566-9999", "2059-10-31");
+        clientController.deleteClientRecord("T-1234-123456-34");
+      		clientController.persistData();
 
-        VehicleRecord v1 = vehicleCatalog.getVehicleRecord("UDF_126");
-        Transaction t1 = new Transaction(clientRecord, v1, "2019-12-23", "2019-12-27", Transaction.Status.Rented);
+      		ClientRecord
+      		clientRecord = new ClientRecord("T-1234-123456-34", 0, "Johny", "Tester",
+      				"(438) 566-9999", "2059-10-31");
+      		clientController.persistData();
+
+      		vehicleCatalog.deleteVehicleRecord("ABE_636");
+      		vehicleCatalog.deleteVehicleRecord("ABD_636");
+      		vehicleCatalog.persistData();
+
+      		VehicleRecord vd1 =
+      				new VehicleRecord(0, 0, "ABE_636", "Sedan", "Audi", "A8", 2011, "Red");
+      		VehicleRecord vd2 =
+      				new VehicleRecord(0, 0, "ABD_636", "Sedan", "Audi", "A8", 2011, "Red");
+      		vehicleCatalog.persistData();
+
+              VehicleRecord v1 = vehicleCatalog.getVehicleRecord("ABE_636");
+        Transaction t1 = new Transaction(0, clientRecord, v1, "2019-12-23", "2019-12-27", Transaction.Status.Rented);
         v1.addTransaction(t1);
+        vehicleCatalog.persistData();
+
+        transactionCatalog.persistData();
         int afteradd = vehicleCatalog.getAvailabilityBetweenDates("2019-12-23", "2019-12-27").size();
 
         System.out.println(afteradd);
@@ -76,16 +139,35 @@ public class TransactionsTests {
         List<VehicleRecord> vr = vehicleCatalog.getAvailabilityBetweenDates("2019-12-23", "2019-12-27");
         int beforeadd = vr.size();
         System.out.println(beforeadd);
-        ClientRecord clientRecord = new ClientRecord("T-1234-123456-12", "Johny", "Tester", "(438) 566-9999", "2059-10-31");
+        clientController.deleteClientRecord("T-1234-123456-34");
+      		clientController.persistData();
 
-        VehicleRecord v1 = vehicleCatalog.getVehicleRecord("ABF_636");
-        Transaction t1 = new Transaction(clientRecord, v1, "2019-12-23", "2019-12-27", Transaction.Status.Rented);
+      		ClientRecord
+      		clientRecord = new ClientRecord("T-1234-123456-34", 0, "Johny", "Tester",
+      				"(438) 566-9999", "2059-10-31");
+      		clientController.persistData();
+
+      		vehicleCatalog.deleteVehicleRecord("ABE_636");
+      		vehicleCatalog.deleteVehicleRecord("ABD_636");
+      		vehicleCatalog.persistData();
+
+      		VehicleRecord vd1 =
+      				new VehicleRecord(0, 0, "ABE_636", "Sedan", "Audi", "A8", 2011, "Red");
+      		VehicleRecord vd2 =
+      				new VehicleRecord(0, 0, "ABD_636", "Sedan", "Audi", "A8", 2011, "Red");
+      		vehicleCatalog.persistData();
+
+              VehicleRecord v1 = vehicleCatalog.getVehicleRecord("ABE_636");
+        Transaction t1 = new Transaction(0, clientRecord, v1, "2019-12-23", "2019-12-27", Transaction.Status.Rented);
         v1.addTransaction(t1);
+        vehicleCatalog.persistData();
+
+        transactionCatalog.persistData();
         int afteradd = vehicleCatalog.getAvailabilityBetweenDates("2019-12-23", "2019-12-27").size();
 
         System.out.println(afteradd);
 
-        Assert.assertFalse(beforeadd == afteradd);
+        Assert.assertFalse(beforeadd != afteradd);
     }
 
     @Test
@@ -93,11 +175,31 @@ public class TransactionsTests {
         List<VehicleRecord> vr = vehicleCatalog.getOverDueParticularDay("2019-10-22");
         int beforeadd = vr.size();
         System.out.println(beforeadd);
-        ClientRecord clientRecord = new ClientRecord("T-1234-123456-12", "Johny", "Tester", "(438) 566-9999", "2059-10-31");
+        clientController.deleteClientRecord("T-1234-123456-34");
+      		clientController.persistData();
 
-        VehicleRecord v1 = vehicleCatalog.getVehicleRecord("ABF_636");
-        Transaction t1 = new Transaction(clientRecord, v1, "2019-10-19", "2019-10-21", Transaction.Status.Rented);
+      		ClientRecord
+      		clientRecord = new ClientRecord("T-1234-123456-34", 0, "Johny", "Tester",
+      				"(438) 566-9999", "2059-10-31");
+      		clientController.persistData();
+
+      		vehicleCatalog.deleteVehicleRecord("ABE_636");
+      		vehicleCatalog.deleteVehicleRecord("ABD_636");
+      		vehicleCatalog.persistData();
+
+      		VehicleRecord vd1 =
+      				new VehicleRecord(0, 0, "ABE_636", "Sedan", "Audi", "A8", 2011, "Red");
+      		VehicleRecord vd2 =
+      				new VehicleRecord(0, 0, "ABD_636", "Sedan", "Audi", "A8", 2011, "Red");
+      		vehicleCatalog.persistData();
+
+              VehicleRecord v1 = vehicleCatalog.getVehicleRecord("ABE_636");       
+
+        Transaction t1 = new Transaction(0, clientRecord, v1, "2019-10-19", "2019-10-21", Transaction.Status.Rented);
         v1.addTransaction(t1);
+        vehicleCatalog.persistData();
+
+        transactionCatalog.persistData();
         int afteradd = vehicleCatalog.getOverDueParticularDay("2019-10-22").size();
 
         System.out.println(afteradd);
@@ -110,15 +212,34 @@ public class TransactionsTests {
         List<VehicleRecord> vr = vehicleCatalog.getOverDueParticularDay("2019-10-22");
         int beforeadd = vr.size();
         System.out.println(beforeadd);
-        ClientRecord clientRecord = new ClientRecord("T-1234-123456-12", "Johny", "Tester", "(438) 566-9999", "2059-10-31");
+        clientController.deleteClientRecord("T-1234-123456-34");
+      		clientController.persistData();
 
-        VehicleRecord v1 = vehicleCatalog.getVehicleRecord("ABF_636");
-        Transaction t1 = new Transaction(clientRecord, v1, "2019-10-19", "2019-10-24", Transaction.Status.Rented);
+      		ClientRecord
+      		clientRecord = new ClientRecord("T-1234-123456-34", 0, "Johny", "Tester",
+      				"(438) 566-9999", "2059-10-31");
+      		clientController.persistData();
+
+      		vehicleCatalog.deleteVehicleRecord("ABE_636");
+      		vehicleCatalog.deleteVehicleRecord("ABD_636");
+      		vehicleCatalog.persistData();
+
+      		VehicleRecord vd1 =
+      				new VehicleRecord(0, 0, "ABE_636", "Sedan", "Audi", "A8", 2011, "Red");
+      		VehicleRecord vd2 =
+      				new VehicleRecord(0, 0, "ABD_636", "Sedan", "Audi", "A8", 2011, "Red");
+      		vehicleCatalog.persistData();
+
+              VehicleRecord v1 = vehicleCatalog.getVehicleRecord("ABE_636");
+        Transaction t1 = new Transaction(0, clientRecord, v1, "2019-10-19", "2019-10-24", Transaction.Status.Rented);
         v1.addTransaction(t1);
+        vehicleCatalog.persistData();
+
+        transactionCatalog.persistData();
         int afteradd = vehicleCatalog.getOverDueParticularDay("2019-10-22").size();
 
         System.out.println(afteradd);
 
         Assert.assertTrue(beforeadd == afteradd);
     }
-}*/
+}
